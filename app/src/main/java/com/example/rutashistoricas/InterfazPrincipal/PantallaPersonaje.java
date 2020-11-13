c4a8282d80d1bbe32dd04f2458a960f07fe6e177
Norberto Fernandez de la Higuera <norbertoalfa@correo.ugr.es>
2020-11-13 20:33:15 +0100

    Algunos errores

--- app/src/main/java/com/example/rutashistoricas/InterfazPrincipal/PantallaPersonaje.java
+++ app/src/main/java/com/example/rutashistoricas/InterfazPrincipal/PantallaPersonaje.java
@@ -10,7 +10,7 @@ import android.view.VelocityTracker;
 import android.view.View;
 import android.widget.TextView;
 
-import com.example.rutashistoricas.ListadoRutas;
+import com.example.rutashistoricas.InterfazPrincipal.ListadoRutas;
 import com.example.rutashistoricas.R;
 
 public class PantallaPersonaje extends AppCompatActivity {
